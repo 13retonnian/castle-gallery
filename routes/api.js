9483f1526c5d406bb5190d89b3721b2fa1851981
13retonnian <13retonnian@protonmail.com>
2021-11-09 18:56:01 -0700

    trying to catch errors and send 404 json

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,8 @@ const app = express()
 const router = express.Router();
 //connect to database
 const db = require('../model/db')
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const { response } = require('express');
 const dotenv = require('dotenv').config()
 
 const castleSchema = new mongoose.Schema({
@@ -21,23 +22,36 @@ const castleSchema = new mongoose.Schema({
 const Castle = mongoose.model('Castle', castleSchema)
 
 router.get('/castles', async (request, response) => {  
-  const castle = await Castle.find()  
-  response.send(castle)
+  try{
+    const castle = await Castle.find()  
+    response.send(castle)
+  } catch (error) {
+    response.send({error: 'Castle Not Found'})
+  }
 })
 
 router.get('/castles/:id', async (req, res) => {
+  try{
   //get full castle list
-  const castles = await Castle.find()
-  // typecast the address bar parameter to number
-  const addressBarParams = Number(req.params.id);
-  //set up find search parameters function payload  
-  const specificCastleParameter = function(castle) {
-    //search parameters
-    return castle.id === addressBarParams;
+    const castles = await Castle.find()
+    // typecast the address bar parameter to number
+    const addressBarParams = Number(req.params.id);
+    //set up find search parameters function payload  
+    const specificCastleParameter = function(castle) {
+      //search parameters
+      return castle.id === addressBarParams;
+    }
+    //find the castle  
+    castle = castles.find(specificCastleParameter)
+    if(!castle) {
+      throw new Error()
+    }
+    //send the castle
+    res.send(castle)
+  } catch (error) {
+    response.status(404)
+    response.send({error: 'Castle Not Found'})
   }
-  //find the castle  
-  castle = castles.find(specificCastleParameter) 
-  res.send(castle)  
 })
 
 module.exports = router
